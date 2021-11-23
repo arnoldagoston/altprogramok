caea2419a3e417d30eb9de6c90be7eae18ee7449
arnoldagoston <91243618+arnoldagoston@users.noreply.github.com>
2021-11-23 09:38:05 +0200

    Add files via upload

---
+++ altprogram_prim/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+bool prim(int a) {
+for (int oszto=2;oszto<a/2+1;oszto++)
+    if(a%oszto==0) {
+        return false;
+    }
+    return true;
+}
+int main()
+{
+    int a;
+    cin >> a;
+    if (prim(a)==1) {
+        cout << "A szam prim";
+    }else{
+    cout << "A szam nem prim";
+    }
+    return 0;
+}
