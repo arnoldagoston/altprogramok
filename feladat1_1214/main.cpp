f452d7282a5df4377c403cbb226cf0defb4e46fa
arnoldagoston <91243618+arnoldagoston@users.noreply.github.com>
2021-12-14 09:30:49 +0200

    Add files via upload

---
+++ feladat1_1214/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+int lnko() {
+    int a1=a, b1=b;
+while (a!=b) {
+        if(a>b) {
+            a-=b;
+        }else {
+        b-=a;}
+    }
+
+    int lnko = a;
+a=a1;
+b=b1;
+return lnko;
+    }
+
+
+int main()
+    {
+        a=4;
+        b=6;
+        cout << lnko() << endl;
+
+    return 0;
+}
